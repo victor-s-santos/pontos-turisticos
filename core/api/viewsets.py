b3d894ba8c4ad38f75109f183eec7c3dfbf3644b
vss-py <victorsantos.py@gmail.com>
2019-01-11 10:34:18 -0200

    returning the previous step

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -2,6 +2,7 @@ from rest_framework.response import Response
 from rest_framework.viewsets import ModelViewSet
 from core.models import PontosTuristicos
 from .serializers import PontosTuristicosSerializer
+from rest_framework.decorators import action
 
 class PontosTuristicosViewSet(ModelViewSet):
     """
@@ -13,28 +14,32 @@ class PontosTuristicosViewSet(ModelViewSet):
     def get_queryset(self):
     	return PontosTuristicos.objects.filter(aprovado = True)
 
-    def list(self, request, *args, **kwargs):
+    #def list(self, request, *args, **kwargs):
     	"""
     	By default the drf returns a list of objects wich are returned by the get_queryset.
     	"""
-    	return Response({'only_a_test': 'it works!'})
+    #	return Response({'only_a_test': 'it works!'})
 
-    def create(self, request, *args, **kwargs):
-    	"""
-    	By this way the drf always returns(post) the follow 
-    	dictionary.  
-    	"""
-    	return Response({'Hello': request.data['nome']})
+"""    def create(self, request, *args, **kwargs):
+        
+        #By this way the drf always returns(post) the follow 
+        #dictionary.  
+        
+        return Response({'Hello': request.data['nome']})
 
 
     def destroy(self, request, *args, **kwargs):
-    	pass
+        pass
 
     def retrieve(self, request, *args, **kwargs):
-    	pass
+        pass
 
     def update(self, request, *args, **kwargs):
-    	pass
+        pass
 
     def partial_update(self, request, *args, **kwargs):
-    	pass
+        pass
+""
+    @action(methods=['get'], detail=True)
+    def denunciar(self, request, pk=None):
+        pass"""
