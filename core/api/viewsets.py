9c60a60697f0bd07f45d258e1e650f6b2426e032
vss-py <victorsantos.py@gmail.com>
2019-01-11 10:55:50 -0200

    overwriting the list method

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -14,32 +14,5 @@ class PontosTuristicosViewSet(ModelViewSet):
     def get_queryset(self):
     	return PontosTuristicos.objects.filter(aprovado = True)
 
-    #def list(self, request, *args, **kwargs):
-    	"""
-    	By default the drf returns a list of objects wich are returned by the get_queryset.
-    	"""
-    #	return Response({'only_a_test': 'it works!'})
-
-"""    def create(self, request, *args, **kwargs):
-        
-        #By this way the drf always returns(post) the follow 
-        #dictionary.  
-        
-        return Response({'Hello': request.data['nome']})
-
-
-    def destroy(self, request, *args, **kwargs):
-        pass
-
-    def retrieve(self, request, *args, **kwargs):
-        pass
-
-    def update(self, request, *args, **kwargs):
-        pass
-
-    def partial_update(self, request, *args, **kwargs):
-        pass
-""
-    @action(methods=['get'], detail=True)
-    def denunciar(self, request, pk=None):
-        pass"""
+    def list(self, request, *args, **kwargs):
+        return Response({'teste': 'Apenas um teste'})
