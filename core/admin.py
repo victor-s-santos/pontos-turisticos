f8496998335c0b076d5eba7b951b42a96dcfbe66
vss-py <victorsantos.py@gmail.com>
2019-02-05 16:56:27 -0200

    adding a custom admin actions

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
 from .models import PontosTuristicos
+from .actions import aprova_comentarios, reprova_comentarios
 
-admin.site.register(PontosTuristicos)
+class PontosTuristicosAdmin(admin.ModelAdmin):
+	list_display = ['nome', 'aprovado']
+	actions = [aprova_comentarios, reprova_comentarios]
+
+admin.site.register(PontosTuristicos, PontosTuristicosAdmin)
 # Register your models here.
